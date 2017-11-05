5b37363b87d07d5681cc9c30443b633cdc121a1b
StarWorker1986 <zhulihongpm@163.com>
2017-11-05 22:15:34 +0800

    CMS-1: Add more initial structure folders

---
+++ admin/src/main/resources/db/cms.sql
@@ -0,0 +1,13 @@
+/*==============================================================*/
+/* DBMS name:	MySQL 5.0                                    	*/
+/* Name:	CMS													*/
+/* Type:	Database											*/
+/* Created on:	2017-10-22                         				*/
+/*==============================================================*/
+
+-- Drop
+DROP DATABASE IF EXISTS starcms;
+
+-- Create
+CREATE DATABASE starcms DEFAULT CHARACTER SET utf8;
+
