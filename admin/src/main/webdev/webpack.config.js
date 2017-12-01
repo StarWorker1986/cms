742d311a145d995a9e78ae266a10be54439a39be
StarWorker1986 <zhulihongpm@163.com>
2017-12-01 22:10:40 +0800

    CMS-1: Structure of front modules

---
+++ admin/src/main/webdev/webpack.config.js
@@ -0,0 +1,21 @@
+﻿const path = require("path");
+const glob = require("glob");
+const srcPath = path.resolve(__dirname, "src/main");
+const buildPath = path.resolve(__dirname, "build");
+const pattern = /([\w\/]+)\/([\w]+)(.js|.css|.html)$/;
+var exports = [];
+
+var matches = glob("**/*(*.js|*.css|*.html)", { cwd: srcPath, sync: true });
+
+for (let i in matches) {
+    let file = matches[i], items = file.match(pattern);
+    exports.push({
+        entry: path.resolve(srcPath, items[0]),
+        output: {
+            path: path.resolve(buildPath, items[1]),
+            filename: items[2] + items[3]
+        }
+    });
+}
+
+module.exports = exports;
